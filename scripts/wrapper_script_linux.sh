fce2cbdc1b9f501319e8beacacf4d3cf92a70a84
Miku AuahDark <lumi.hotaru@gmail.com>
2021-08-07 12:31:22 +0800

    Fix packaging.

--- scripts/wrapper_script_linux.sh
+++ scripts/wrapper_script_linux.sh
@@ -3,7 +3,7 @@
 # https://github.com/pfirsich/love-appimages/blob/b58084eb23f6595617ad9511b5b136a7523e2f4d/build.py#L46
 
 cd "$OWD"
-love_files=$(find $APPDIR/share -type f -name lovegame)
+love_files=$(find $APPDIR/share -type d -name lovegame)
 if [ -z "$love_files" ]; then
 	$APPDIR/bin/love "$@"
 else
